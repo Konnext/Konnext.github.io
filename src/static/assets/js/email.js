d8113d1d9e38fbd13660bef5f611e0dc7c4ca1b6
Konnext <92968144+Konnext@users.noreply.github.com>
2021-11-27 18:22:22 +1300

    update

---
+++ src/static/assets/js/email.js
@@ -0,0 +1,22 @@
+// import { SMTPClient } from "emailjs";
+
+// const client = new SMTPClient({
+//     user: "user",
+//     password: "password",
+//     host: "smtp.your-email.com",
+//     ssl: true,
+// });
+
+// //开始发送邮件
+// client.send(
+//     {
+//         text: "i hope this works",
+//         from: "you <username@your-email.com>",
+//         to: "someone <someone@your-email.com>, another <another@your-email.com>",
+//         cc: "else <else@your-email.com>",
+//         subject: "testing emailjs",
+//     },
+//     (err, message) => {
+//         console.log(err || message);
+//     }
+// );
