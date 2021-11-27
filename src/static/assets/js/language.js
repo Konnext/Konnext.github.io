82d771dd7e99939585d3f622627a36fc30374c48
Konnext <92968144+Konnext@users.noreply.github.com>
2021-11-27 23:59:19 +1300

    update

---
+++ src/static/assets/js/language.js
@@ -0,0 +1,11 @@
+// import {
+//     $,
+//     $doc,
+//     isInViewport,
+//     throttleScroll,
+// } from "../../../js/parts/_utility";
+// console.log($doc);
+
+// // let selectsssss = $doc.querySelector("#selec");
+// console.log(selectsssss);
+// console.log("=============");
